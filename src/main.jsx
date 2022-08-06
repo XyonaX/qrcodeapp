88cb8a67695dab6abd79a635e875e69b7ba92e9a
XyonaX <zaxttpro@gmail.com>
2022-08-06 13:28:32 -0300

    index docs actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename='/vite-react-starter'>
     <App />
   </React.StrictMode>
 )
